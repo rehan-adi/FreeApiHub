b47ecd976de05974be16fd4fa19a54f093301892
Rehan <rehanalire52@gmail.com>
2024-10-04 22:08:19 +0530

    Add Signup card with shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Signin from "./pages/Signin";
+import { Signup } from "./pages/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/signin" element={<Signin />} />
+        <Route path="/signin" element={<Signup />} />
       </Routes>
     </Router>
   );
