80ce1f079fac00948fc5b1a8bf3fe1fea0459c37
Rehan <rehanalire52@gmail.com>
2024-10-04 22:24:39 +0530

    Add sonner toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { Toaster } from 'sonner';
 import { Signup } from "./pages/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
+    <>
+      <Toaster />
+      <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
+    </>
   );
 };
 
--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,20 +1,29 @@
 import axios from "axios";
+import { toast } from "sonner";
 
 export const useSignup = () => {
+  const signUp = async (data: any) => {
+    try {
+      const response = await axios.post("/api/signup", data);
 
-    const signUp = async(data: any) => {
-        try {
-            const response = await axios.post('/api/signup', data);
-
-            if(response.status === 200) {
-                console.log('User registered successfully');
-            }
-
-        } catch (error) {
-             console.log(error);
-        }
+      if (response.status === 200) {
+        console.log("User registered successfully");
+        toast.success("Signup Successful", {
+          description: response.data.message,
+          duration: 2000,
+        });
+      }
+      
+    } catch (error: any) {
+      console.log(error);
+      toast.error("Signup Failed", {
+        description:
+          error.response?.data?.message ||
+          "An error occurred. Please try again.",
+      });
     }
-    return {
-        signUp,
-    }
-}
+  };
+  return {
+    signUp,
+  };
+};
