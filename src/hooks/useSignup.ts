9df8e6a5941317c81627147b892047d79c5ff82b
Rehan <rehanalire52@gmail.com>
2024-10-05 18:47:26 +0530

    Add useSignup hook with loading state for user registration.

--- src/hooks/useSignup.ts
+++ src/hooks/useSignup.ts
@@ -1,19 +1,26 @@
 import axios from "axios";
 import { toast } from "sonner";
+import { useState } from "react";
+import { SignupValidationType } from "@/validations/auth.validation";
 
 export const useSignup = () => {
-  const signUp = async (data: any) => {
-    try {
-      const response = await axios.post("/api/signup", data);
+  const [loading, setLoading] = useState(false);
 
-      if (response.status === 200) {
+  const signUp = async (data: SignupValidationType) => {
+    setLoading(true);
+    try {
+      const response = await axios.post(
+        "https://api.freeapi.app/api/v1/users/register",
+        data
+      );
+
+      if (response.status === 201) {
         console.log("User registered successfully");
         toast.success("Signup Successful", {
           description: response.data.message,
           duration: 2000,
         });
       }
-      
     } catch (error: any) {
       console.log(error);
       toast.error("Signup Failed", {
@@ -21,9 +28,12 @@ export const useSignup = () => {
           error.response?.data?.message ||
           "An error occurred. Please try again.",
       });
+    } finally {
+       setLoading(false);
     }
   };
   return {
     signUp,
+    loading,
   };
 };
