67df13c90c67b02767134e6273a0d683e7cffd60
Rehan <rehanalire52@gmail.com>
2024-10-06 12:56:20 +0530

    Add products route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from 'sonner';
 import { Signup } from "./pages/Signup";
 import { Signin } from './pages/Signin';
+import Products from './pages/Products';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </Router>
     </>
