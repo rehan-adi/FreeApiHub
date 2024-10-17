c0d248d3189d166c4d7543d48e307dfc2f5c5323
Rehan <rehanalire52@gmail.com>
2024-10-17 22:25:52 +0530

    Add lucid-react and add bg color to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body className=" bg-black text-white antialiased">
         <Navbar />
         {children}
       </body>
