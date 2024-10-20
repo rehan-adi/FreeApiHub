ac2a61ee52d14afec694f1d8d4956b63c3e0705e
Rehan <rehanalire52@gmail.com>
2024-10-20 09:37:43 +0530

    Add express server with Middleware and health check route

--- apps/freeapihub-be/src/app.ts
+++ apps/freeapihub-be/src/app.ts
@@ -1,11 +1,24 @@
+import hpp from 'hpp';
+import env from'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
 import express, { Request, Response } from 'express';
+
+env.config();
 
 const app = express();
 
+// Middleware 
+app.use(express.json());
+app.use(hpp());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// health check route
 app.get("/", (req: Request, res: Response) => {
     return res.status(200).json({ success: true, message: "Okay" });
 });
 
-app.listen(8080, () => {
-    console.log('Server is running');
-})
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT}`);
+});
