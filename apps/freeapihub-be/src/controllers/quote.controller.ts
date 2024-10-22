b59ca9b9fe3a09f5f8a4577a8c8e4fe7514a71ea
Rehan <rehanalire52@gmail.com>
2024-10-22 15:44:58 +0530

    feat: add submitQuoteData controller and routes for creating quotes

--- apps/freeapihub-be/src/controllers/quote.controller.ts
+++ apps/freeapihub-be/src/controllers/quote.controller.ts
@@ -5,17 +5,60 @@ export const getQuotes = async (req: Request, res: Response) => {
   try {
     const quotes = await prisma.quote.findMany();
 
-    return res
-      .status(200)
-      .json({
-        success: true,
-        data: quotes,
-        message: "All quotes retrieved successfully",
-      });
+    return res.status(200).json({
+      success: true,
+      data: quotes,
+      message: "All quotes retrieved successfully",
+    });
   } catch (error) {
     const message =
       error instanceof Error ? error.message : "Unknown error occurred";
     console.error("Error fetching quotes:", message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: message,
+    });
+  }
+};
+
+export const submitQuoteData = async (req: Request, res: Response) => {
+  try {
+    const { author, content, rate, likes, dislikes, category } = req.body;
+
+    const existingQuote = await prisma.quote.findFirst({
+      where: {
+        content,
+      },
+    });
+
+    if (existingQuote) {
+      return res.status(400).json({
+        success: false,
+        message: "Quote with this content already exists",
+      });
+    }
+
+    const newQuote = await prisma.quote.create({
+      data: {
+        author,
+        content,
+        rate,
+        likes,
+        dislikes,
+        category,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: newQuote,
+      message: "Quote created successfully",
+    });
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error("Error submiing quote:", message);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
--- apps/freeapihub-be/src/routes/quote.route.ts
+++ apps/freeapihub-be/src/routes/quote.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { getQuotes } from "src/controllers/quote.controller";
+import { getQuotes, submitQuoteData } from "../controllers/quote.controller";
 
 export const quoteRouer = express.Router();
 
 quoteRouer.get("/", getQuotes);
+quoteRouer.post("/create", submitQuoteData);
