aa9849a711f0f34b2d55f746e099257387507540
Rehan <rehanalire52@gmail.com>
2024-10-22 18:38:38 +0530

    publishedAt: new Date(publishedAt),

--- apps/freeapihub-be/src/controllers/book.controller.ts
+++ apps/freeapihub-be/src/controllers/book.controller.ts
@@ -5,17 +5,73 @@ export const getBooks = async (req: Request, res: Response) => {
   try {
     const books = await prisma.book.findMany();
 
-    return res
-      .status(200)
-      .json({
-        success: false,
-        data: books,
-        message: "All books retrieved successfully",
-      });
+    return res.status(200).json({
+      success: true,
+      data: books,
+      message: "All books retrieved successfully",
+    });
   } catch (error) {
     const message =
       error instanceof Error ? error.message : "Unknown error occurred";
     console.error("Error fetching books:", message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: message,
+    });
+  }
+};
+
+export const submitBookData = async (req: Request, res: Response) => {
+  try {
+    const {
+      name,
+      author,
+      description,
+      publisher,
+      publishedAt,
+      genre,
+      pages,
+      imageUrl,
+      rating,
+    } = req.body;
+
+    const existingBook = await prisma.book.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    if (existingBook) {
+      return res.status(400).json({
+        success: false,
+        message: "Book with this name already exists",
+      });
+    }
+
+    const newBook = await prisma.book.create({
+      data: {
+        name,
+        author,
+        description,
+        publisher,
+        publishedAt,
+        genre,
+        pages,
+        imageUrl,
+        rating,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      data: newBook,
+      message: "Books created successfully",
+    });
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error("Error submiting books:", message);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
--- apps/freeapihub-be/src/routes/book.route.ts
+++ apps/freeapihub-be/src/routes/book.route.ts
@@ -1,6 +1,7 @@
-import express from 'express'
-import { getBooks } from '../controllers/book.controller';
+import express from "express";
+import { getBooks, submitBookData } from "../controllers/book.controller";
 
 export const bookRouter = express.Router();
 
 bookRouter.get("/", getBooks);
+bookRouter.post("/create", submitBookData);
