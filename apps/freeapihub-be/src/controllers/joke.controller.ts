80dcee9f0dbba35a3967e4c19b080d81940273a4
Rehan <rehanalire52@gmail.com>
2024-10-23 23:34:32 +0530

    feat: add getJokeById route and controller

--- apps/freeapihub-be/src/controllers/joke.controller.ts
+++ apps/freeapihub-be/src/controllers/joke.controller.ts
@@ -7,11 +7,42 @@ export const getJokes = async (req: Request, res: Response) => {
 
     return res
       .status(200)
-      .json({ success: true, data: jokes, message: "ohh no" });
+      .json({ success: true, data: jokes, message: "All jokes retrieved successfully" });
   } catch (error: unknown) {
     const message =
       error instanceof Error ? error.message : "Unknown error occurred";
     console.error("Error fetching jokes:", message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: message,
+    });
+  }
+};
+
+export const getJokeById = async (req: Request, res: Response) => {
+  try {
+    const { jokeId } = req.params;
+
+    const existingJoke = await prisma.joke.findUnique({
+      where: {
+        id: jokeId,
+      },
+    });
+
+    if (!existingJoke) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Joke not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, data: existingJoke, message: "Joke retrieved successfully" });
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error("Error fetching joke:", message);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
--- apps/freeapihub-be/src/routes/joke.route.ts
+++ apps/freeapihub-be/src/routes/joke.route.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   deleteJokeData,
   deleteJokeDataById,
+  getJokeById,
   getJokes,
   submitJokeData,
 } from "../controllers/joke.controller";
@@ -9,6 +10,7 @@ import {
 export const jokeRouter = express.Router();
 
 jokeRouter.get("/", getJokes);
+jokeRouter.get("/:jokeId", getJokeById);
 jokeRouter.post("/create", submitJokeData);
 jokeRouter.delete("/delete-all", deleteJokeData);
 jokeRouter.delete("/delete", deleteJokeDataById);
