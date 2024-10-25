227c70c7bf90b815afa2971ee46c1bf07890f956
Rehan <rehanalire52@gmail.com>
2024-10-25 19:36:37 +0530

    feat: Implement endpoint to retrieve a stock by its ID

--- apps/freeapihub-be/src/controllers/stock.controller.ts
+++ apps/freeapihub-be/src/controllers/stock.controller.ts
@@ -1,23 +1,54 @@
 import prisma from "@freeapihub/db/index";
 import { Request, Response } from "express";
 
-export const getStocks = async(req: Request, res: Response) => {
-    try {
-        const stocks = await prisma.stock.findMany();
-    
-        return res.status(200).json({
-          success: true,
-          data: stocks,
-          message: "All stocks retrieved successfully",
-        });
-      } catch (error) {
-        const message =
-          error instanceof Error ? error.message : "Unknown error occurred";
-        console.error("Error fetching stocks:", message);
-        return res.status(500).json({
-          success: false,
-          message: "Internal Server Error",
-          error: message,
-        });
-      }
-}
+export const getStocks = async (req: Request, res: Response) => {
+  try {
+    const stocks = await prisma.stock.findMany();
+
+    return res.status(200).json({
+      success: true,
+      data: stocks,
+      message: "All stocks retrieved successfully",
+    });
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error("Error fetching stocks:", message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: message,
+    });
+  }
+};
+
+export const getStockById = async (req: Request, res: Response) => {
+  try {
+    const { stockId } = req.params;
+
+    const existingStock = await prisma.stock.findUnique({
+      where: {
+        id: stockId,
+      },
+    });
+
+    if (!existingStock) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Stock not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, data: existingStock, message: "Stock data" });
+  } catch (error) {
+    const message =
+      error instanceof Error ? error.message : "Unknown error occurred";
+    console.error("Error fetching stock:", message);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+      error: message,
+    });
+  }
+};
--- apps/freeapihub-be/src/routes/stock.route.ts
+++ apps/freeapihub-be/src/routes/stock.route.ts
@@ -1,6 +1,7 @@
-import express from 'express';
-import { getStocks } from '../controllers/stock.controller';
+import express from "express";
+import { getStockById, getStocks } from "../controllers/stock.controller";
 
 export const stockRouter = express.Router();
 
 stockRouter.get("/", getStocks);
+stockRouter.get("/:stockId", getStockById);
