d2139cbab4818fe81c80d9a525d0bf270d6bea09
Rehan <rehanalire52@gmail.com>
2024-10-27 21:25:21 +0530

    Remove font and clean up svg's

--- apps/freeapihub-docs/app/layout.tsx
+++ apps/freeapihub-docs/app/layout.tsx
@@ -1,19 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "FreeApiHub",
@@ -27,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <Navbar />
         {children}
         <Footer />
