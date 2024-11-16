119c6791441e424a2ccfe1de2f55cc8f8eeebfbe
Rehan <rehanalire52@gmail.com>
2024-11-16 13:08:24 +0530

    Update on Navbar

--- apps/freeapihub-docs/components/Navbar.tsx
+++ apps/freeapihub-docs/components/Navbar.tsx
@@ -6,20 +6,16 @@ import ThemeToggle from "./ThemeToggle";
 import { signOut } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
 import { useSession } from "next-auth/react";
-import { PanelRightOpen, X } from "lucide-react";
-import { PiLineVerticalBold,  } from "react-icons/pi";
+import { Button } from "@/components/ui/button";
+import { PiLineVerticalBold } from "react-icons/pi";
 
 const Navbar = () => {
   const { data: session } = useSession();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setIsSidebarOpen((prev) => !prev);
-  };
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-      <nav className="py-3.5 flex justify-between fixed top-0 z-50 w-full border-b dark:border-white dark:border-opacity-10 border-black border-opacity-15 backdrop-blur items-center md:px-24 px-5">
+      <nav className="md:py-4 py-3.5 flex justify-between fixed top-0 z-50 w-full border-b dark:border-white dark:border-opacity-10 border-black border-opacity-15 backdrop-blur items-center md:px-44 px-5">
         <span className="flex justify-between text-black dark:text-white text-base font-semibold items-center">
           <svg
             fill="currentColor"
@@ -35,9 +31,9 @@ const Navbar = () => {
           </svg>
           FreeApiHub
         </span>
-        <div className="flex justify-between items-center gap-3">
+        <div className="flex justify-center items-center gap-3">
           <div className="flex justify-between items-center gap-2">
-            <ThemeToggle />
+            <ThemeToggle  />
             <Link
               href="https://github.com/rehan-adi/FreeApiHub"
               target="_blank"
@@ -69,19 +65,50 @@ const Navbar = () => {
             )}
           </div>
           {/* Sidebar Toggle Button */}
-          <button onClick={toggleSidebar} className="md:hidden md:ml-0 ml-2">
-            <PanelRightOpen className="text-2xl dark:text-white text-black" />
-          </button>
+          <Button
+            className="group flex md:hidden"
+            variant="outline"
+            size="icon"
+            onClick={() => setOpen((prevState) => !prevState)}
+            aria-expanded={open}
+            aria-label={open ? "Close menu" : "Open menu"}
+          >
+            <svg
+              className="pointer-events-none dark:text-white text-black"
+              width={16}
+              height={16}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4 12L20 12"
+                className="origin-center -translate-y-[7px] transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-x-0 group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[315deg]"
+              />
+              <path
+                d="M4 12H20"
+                className="origin-center transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.8)] group-aria-expanded:rotate-45"
+              />
+              <path
+                d="M4 12H20"
+                className="origin-center translate-y-[7px] transition-all duration-300 [transition-timing-function:cubic-bezier(.5,.85,.25,1.1)] group-aria-expanded:translate-y-0 group-aria-expanded:rotate-[135deg]"
+              />
+            </svg>
+          </Button>
         </div>
       </nav>
 
       {/* Sidebar Component */}
       <div
-        className={`fixed top-0 right-0 w-full px-5 rounded-xl py-6 z-50 h-screen shadow-lg bg-white dark:bg-[#141312] transition-transform duration-300 transform ${
-          isSidebarOpen ? "translate-x-0" : "translate-x-full"
+         className={`fixed top-20 transition-transform duration-300 transform z-50 md:w-full w-[90%] px-5 py-6 rounded-xl border dark:border-opacity-15 border-opacity-25 dark:border-white border-black shadow-lg bg-white dark:bg-black ${
+          open ? "right-[50%] translate-x-[50%]" : "right-0 translate-x-full"
         } md:hidden`}
       >
-        <div className="p-5 border dark:border-white border-black dark:border-opacity-10 border-opacity-20">
+        <div className="p-2">
           <div className="flex justify-between items-center">
             <span className="flex justify-between text-black dark:text-white text-base font-semibold items-center">
               <svg
@@ -96,24 +123,32 @@ const Navbar = () => {
                 <path d="M-50.42 40.054L-72-27h30l21.58 67.054z"></path>
                 <path d="M-100 60l28-87h30l-28 87z"></path>
               </svg>
-              FreeApiHub
+              FreeApiHub1
             </span>
-            <span onClick={toggleSidebar}>
-              <X className="text-black dark:text-white"/>
-            </span>
+            <div className="">
+              <Link
+                href="https://github.com/rehan-adi/FreeApiHub"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="px-2 py-1.5 rounded-md dark:hover:bg-neutral-800 cursor-pointer hover:bg-gray-200">
+                  <FaGithub className="text-black text-2xl dark:text-white" />
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className="flex flex-col mt-8 gap-3">
-          <div className="mb-6">
-            <Link
-              href="https://github.com/rehan-adi/FreeApiHub"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div className="px-2 py-1.5 rounded-md dark:hover:bg-neutral-800 cursor-pointer hover:bg-gray-200">
-                <FaGithub className="text-black text-2xl dark:text-white" />
-              </div>
-            </Link>
-          </div>
+          <div className="flex flex-col mt-16 gap-3">
+            {/* <div className="mb-6">
+              <Link
+                href="https://github.com/rehan-adi/FreeApiHub"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <div className="px-2 py-1.5 rounded-md dark:hover:bg-neutral-800 cursor-pointer hover:bg-gray-200">
+                  <FaGithub className="text-black text-2xl dark:text-white" />
+                </div>
+              </Link>
+            </div> */}
             {session?.user ? (
               <button
                 onClick={() => signOut()}
